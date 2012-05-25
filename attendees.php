d38514a37dcbfd89e472996d5df16a2e290c502f
Mark DiMarco <mark.dimarco@gmail.com>
2012-05-25 10:31:30 -0700

    twitter changes

---
+++ attendees.php
@@ -0,0 +1,32 @@
+<?php 
+
+require_once "../_/scripts/grab.php";
+
+$con = mysql_connect($host,$user,$pass);
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+ mysql_select_db($db, $con); 
+ 
+
+$sql="SELECT * FROM twitter";
+
+$res = mysql_query($sql, $con);
+if (!$res)
+  {
+  die('Error: ' . mysql_error());
+  }
+header("Content-type: application/json");
+
+
+$rows = array();
+while($data = mysql_fetch_assoc($res)) {
+	$rows[] = $data;
+}
+echo json_encode($rows);
+
+
+mysql_close($con);
+?>
+
