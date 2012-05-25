54ff62732ee351a5e0a151e7eba6f56511e2d405
markmarkoh <mark.dimarco@gmail.com>
2012-05-25 18:23:06 -0500

    max steps fix

--- js/game.js
+++ js/game.js
@@ -7,11 +7,13 @@
             prevScrollPos = 0,
             step = 1,
             isMobile = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/),
-            maxSteps = _.isUndefined(isMobile) ? 25 : 55;
+            maxSteps = isMobile ? 25 : 55,
             rangeY = d3.scale.linear().range([0, maxSteps]);
 
 
         window.rangeY = rangeY;
+
+        alert(maxSteps);
 
         var updateOnScroll = function() {
             currentScrollPos = $body.scrollTop();
